06c6ff59d6ed5ab6a0199b6d644d7cf5b67aaaaa
Imantas <precasimantas@gmail.com>
2022-03-14 16:47:03 +0200

    balance

--- files.js
+++ files.js
@@ -3,10 +3,25 @@ console.clear();
 const fs = require("fs/promises");
 
 (async () => {
-	const drinks = await fs.readFile("./data/drinks.txt", "utf-8");
-	console.log(drinks);
+	const drinksContent = await fs.readFile("./data/drinks.txt", "utf-8");
+	const drinksList = drinksContent.split("\r\n");
 
-	for (const letter of drinks) {
-		console.log(letter);
+	//Pasirinkimas1: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+	//Pasirinkimas1: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+	//Pasirinkimas1: [gerimo-pavadinimas] kurio kaina yra [kaina] EUR.
+
+	let i = 0;
+	for (const content of drinksList) {
+		const drinkDetails = content.split(" ");
+		const [name, price, count] = drinkDetails; //Destrukturizavimas
+		const balance = +price * +count;
+		const b1 = +balance.toFixed(2);
+		const b2 = Math.round(balance * 100) / 100;
+
+		console.log(
+			`Pasirinkimas ${++i} ${name} kurio kaina yra ${price} EUR buvo parduota ${count} ir bendra apyvarta yra ${b2.toFixed(
+				2
+			)} EUR.`
+		);
 	}
 })();
